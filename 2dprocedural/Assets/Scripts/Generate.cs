c5d7a83ac4f9b7b777cec626c8a749ee7af8a3ba
Rishabh Sharma <rishsharma13@gmail.com>
2017-04-17 23:41:37 +1000

    terrain generation works for specified terrain blocks

---
+++ 2dprocedural/Assets/Scripts/Generate.cs
@@ -0,0 +1,49 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Generate : MonoBehaviour {
+
+
+	public GameObject dirtObject;
+	public GameObject grassObject;
+
+	public int width;
+	public float heightFactor;
+	public int dirtThreshold;
+	public float smoothness;
+	public int heigthAdd;
+
+	[HideInInspector]
+	public float randSeed;
+
+
+	void Start () {
+		GeneratePlatfrom ();
+	}
+	
+
+	public void GeneratePlatfrom() {
+
+		for(int i=0; i < width; i++) {
+
+			int h = Mathf.RoundToInt(Mathf.PerlinNoise(randSeed,(i+transform.position.x)/smoothness)*heightFactor)+heigthAdd;
+
+			for(int j=0; j < h; j++) {
+				
+				GameObject newTile;
+
+				if (j < h - dirtThreshold) {
+					newTile = dirtObject;
+				} else {
+					newTile = grassObject;
+				}
+
+				Instantiate(newTile,new Vector3(i+transform.position.x,j), Quaternion.identity);
+
+			}
+		}
+
+
+	}
+}
---
+++ 2dprocedural/Assets/Scripts/GenerateTerrain.cs
@@ -0,0 +1,31 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class GenerateTerrain : MonoBehaviour {
+
+
+	public GameObject terrainGenerator;
+	int terrainBlockWidth;
+	public int numTerrainBlocks;
+	float randSeed;
+
+
+	void Start () {
+		terrainBlockWidth = terrainGenerator.GetComponent<Generate> ().width;
+		randSeed = Random.Range (-1000f, 1000);
+		CreateTerrain ();
+		
+	}
+	
+	void CreateTerrain () {
+
+		int lastX = -terrainBlockWidth;
+
+		for(int i=0;i<numTerrainBlocks;i++) {
+			GameObject newTerrainBlock = Instantiate(terrainGenerator,new Vector3(lastX+terrainBlockWidth,0f),Quaternion.identity) as GameObject;
+			newTerrainBlock.GetComponent<Generate>().randSeed = randSeed;
+			lastX += terrainBlockWidth;
+		}
+	}
+}
